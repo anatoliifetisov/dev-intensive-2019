a61755a51c16b154f469d0fa1e7e909dfc7aed92
Anatolii Fetisov <anatolii.fetisov@icloud.com>
2019-07-10 22:35:17 +0200

    Fix base message

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -2,32 +2,24 @@ package ru.skillbranch.devintensive.models
 
 import java.util.*
 
-abstract class BaseMessage(
+abstract class BaseMessage (
     val id: String,
     val from: User?,
     val chat: Chat,
     val isIncoming: Boolean = false,
-    val date: Date = Date()
+    val date : Date = Date()
 ) {
 
     abstract fun formatMessage(): String
 
-    companion object AbstractFactory {
+    companion object AbstractFactory{
+        var lastId = 0
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type:String = "text", payload: Any, isIncoming: Boolean = false):BaseMessage{
+            return when(type){
+                "image"-> ImageMessage("${lastId++}", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                "text" -> TextMessage("${lastId++}", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
 
-        private var lastId = 0;
-        fun makeMessage(
-            from: User?,
-            chat: Chat,
-            date: Date = Date(),
-            type: String = "text",
-            payload: Any,
-            isIncoming: Boolean = false
-        ): BaseMessage {
-            lastId += 1
-            return when (type) {
-                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
-                "text" -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
-                else -> throw IllegalArgumentException()
+                else-> if ("image" == payload || "text" == payload) makeMessage(from, chat, date, payload, type, isIncoming) else throw IllegalArgumentException()
             }
         }
     }
